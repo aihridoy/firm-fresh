3c5fdaeb42344048233f547cab68e5aa27c02b23
aihridoy <cse_1912020168@lus.ac.bd>
2025-10-15 00:24:34 +0600

    init: featured products

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,4 +1,5 @@
 import Categories from "@/components/Categories";
+import FeaturedProducts from "@/components/FeaturedProducts";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Categories />
+      <FeaturedProducts />
     </>
   );
 }

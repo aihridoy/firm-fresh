4d43b9fb862cf3772ddc92612d315585a045dffa
aihridoy <cse_1912020168@lus.ac.bd>
2025-10-15 00:29:47 +0600

    init: choose & newsletter

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,7 +1,9 @@
 import Categories from "@/components/Categories";
+import Choose from "@/components/Choose";
 import FeaturedProducts from "@/components/FeaturedProducts";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Newsletter from "@/components/Newsletter";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <Hero />
       <Categories />
       <FeaturedProducts />
+      <Choose />
+      <Newsletter />
     </>
   );
 }
